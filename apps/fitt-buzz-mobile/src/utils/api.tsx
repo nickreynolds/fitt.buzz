8e3dd7103639703e96b34f8567ff93dbd74190e2
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2025-01-16 19:45:38 -0500

    chore: insane client logging

--- apps/fitt-buzz-mobile/src/utils/api.tsx
+++ apps/fitt-buzz-mobile/src/utils/api.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
@@ -38,6 +39,7 @@ export function TRPCProvider(props: { children: React.ReactNode }) {
             headers.set("x-trpc-source", "expo-react");
 
             const token = getToken();
+            Alert.alert("token", token ?? "none");
             if (token) headers.set("Authorization", `Bearer ${token}`);
 
             return Object.fromEntries(headers);

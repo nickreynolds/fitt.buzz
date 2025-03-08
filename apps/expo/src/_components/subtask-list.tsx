deb64637763936fd9235597f05159f56fc7babf1
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2025-03-08 17:17:21 -0500

    feat: add drag and drop list to mobile

--- apps/expo/src/_components/subtask-list.tsx
+++ apps/expo/src/_components/subtask-list.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Text, View } from "react-native";
+import DraggableFlatList, {
+  ScaleDecorator,
+} from "react-native-draggable-flatlist";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 import type { RouterOutputs } from "@acme/api";
 
@@ -12,6 +16,7 @@ interface SubtaskListProps {
 }
 
 export function SubtaskList({ initialTask, parentTaskId }: SubtaskListProps) {
+  const utils = api.useUtils();
   const { data: task } = api.task.getTask.useQuery(
     { id: parentTaskId },
     { initialData: initialTask },
@@ -30,11 +35,80 @@ export function SubtaskList({ initialTask, parentTaskId }: SubtaskListProps) {
     );
   }
 
+  const renderItem = ({
+    item,
+    drag,
+    isActive,
+  }: {
+    item: RouterOutputs["task"]["getTask"];
+    drag: () => void;
+    isActive: boolean;
+  }) => {
+    return (
+      <ScaleDecorator>
+        <TouchableOpacity onLongPress={drag}>
+          <TaskCard key={item?.id} task={item} />
+        </TouchableOpacity>
+      </ScaleDecorator>
+    );
+  };
+
+  const mutateTaskOrder = api.task.reorderTasks.useMutation({
+    onMutate: (data) => {
+      const oldChildTasks = utils.task.getTask.getData({ id: parentTaskId });
+      const dataMapping = new Map<string, number>();
+      for (const order of data) {
+        dataMapping.set(order.id, order.sortIndex);
+      }
+
+      const newChildTasks = oldChildTasks?.childTasks?.map((t) => {
+        return {
+          ...t,
+          sortIndex: dataMapping.get(t.id) ?? t.sortIndex,
+        };
+      });
+
+      const sortedChildTasks = newChildTasks?.sort(
+        (a, b) => a.sortIndex - b.sortIndex,
+      );
+
+      utils.task.getTask.setData(
+        { id: parentTaskId },
+        {
+          ...task,
+          childTasks: sortedChildTasks,
+        },
+      );
+    },
+    onSettled: async () => {
+      // await utils.task.getTask.refetch({ id: parentTaskId });
+    },
+  });
+
   return (
     <View className="mt-4 space-y-2">
-      {tasks.map((t) => {
-        return <TaskCard key={t.id} task={t} />;
-      })}
+      <DraggableFlatList
+        data={tasks.sort((a, b) => a.sortIndex - b.sortIndex)}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        onDragEnd={({ data }) => {
+          // console.log("f: ", JSON.stringify(data, null, 2));
+          const taskOrder: { id: string; sortIndex: number }[] = [];
+          data.forEach((task, index) => {
+            if (task.id && task.sortIndex !== index) {
+              // utils.task.getTask.setData(
+              //   { id: task.id },
+              //   {
+              //     ...task,
+              //     sortIndex: index,
+              //   },
+              // );
+              taskOrder.push({ id: task.id, sortIndex: index });
+            }
+          });
+          mutateTaskOrder.mutate(taskOrder);
+        }}
+      />
     </View>
   );
 }
--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import "@bacons/text-decoder/install";
 
+import { Platform, UIManager } from "react-native";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { PortalHost } from "@rn-primitives/portal";
@@ -8,14 +9,24 @@ import { TRPCProvider } from "~/utils/api";
 
 import "../styles.css";
 
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+if (Platform.OS === "android") {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
+
 // This is the main layout of the app
 // It wraps your pages with the providers they need
 export default function RootLayout() {
   return (
     <TRPCProvider>
-      <Slot />
-      <StatusBar />
-      <PortalHost />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Slot />
+        <StatusBar />
+        <PortalHost />
+      </GestureHandlerRootView>
     </TRPCProvider>
   );
 }
